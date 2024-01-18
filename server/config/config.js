6028fea6be9314b21a82ebc9fb1e76beed31506d
Arshil Vahora <70322256+ARSHIL1804@users.noreply.github.com>
2024-01-18 21:20:21 +0530

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     'PORT' : 3000,
-    'JWT_SECRET': 'R4ND0M5TR1NG',
-    'MONGO_URL':'mongodb+srv://arshil1804:Arshil%401804@cluster0.pjttaga.mongodb.net/?retryWrites=true&w=majority'
+    'JWT_SECRET': 'R4ND0M5TR1NG',  // random string for password hash
+    'MONGO_URL': '' //PASTE YOUR MONGO URL HERE 
 }
 
 module.exports = config;
